7045a3c5560c43407135585313d75ad3e8d82868
unknown <vuxuanthanh2k@gmail.com>
2021-08-08 23:25:57 +0700

    add models

---
+++ DoNgoaiChinhHang/DoNgoaiChinhHang/Models/CategoryBase.cs
@@ -0,0 +1,34 @@
+namespace DoNgoaiChinhHang.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("CategoryBase")]
+    public partial class CategoryBase
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public CategoryBase()
+        {
+            Categories = new HashSet<Category>();
+            Categories1 = new HashSet<Category>();
+        }
+
+        [StringLength(10)]
+        public string CategoryBaseID { get; set; }
+
+        [StringLength(50)]
+        public string CategoryBaseName { get; set; }
+
+        [StringLength(4000)]
+        public string Description { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Category> Categories { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Category> Categories1 { get; set; }
+    }
+}
---
+++ DoNgoaiChinhHang/DoNgoaiChinhHang/Models/Product.cs
@@ -0,0 +1,64 @@
+namespace DoNgoaiChinhHang.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Product")]
+    public partial class Product
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Product()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+            OrderDetails1 = new HashSet<OrderDetail>();
+        }
+
+        [StringLength(10)]
+        public string ProductID { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string CategoryID { get; set; }
+
+        [StringLength(100)]
+        public string ProductName { get; set; }
+
+        [StringLength(200)]
+        public string Image { get; set; }
+
+        public int? Evaluation { get; set; }
+
+        [StringLength(12)]
+        public string Brand { get; set; }
+
+        [StringLength(50)]
+        public string Orgin { get; set; }
+
+        public float? Price { get; set; }
+
+        [StringLength(500)]
+        public string Benefit { get; set; }
+
+        [StringLength(4000)]
+        public string Introduction { get; set; }
+
+        [StringLength(500)]
+        public string Element { get; set; }
+
+        [StringLength(50)]
+        public string Tutorial { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual Category Category1 { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OrderDetail> OrderDetails1 { get; set; }
+    }
+}
