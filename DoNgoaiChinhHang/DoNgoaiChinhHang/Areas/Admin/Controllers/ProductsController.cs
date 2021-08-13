05417fee7d9ec1dd81f6432c4f77ed3d7425aa34
Long Duong <code-cung-duong>
2021-08-13 10:29:44 +0700

    crud products

--- DoNgoaiChinhHang/DoNgoaiChinhHang/Areas/Admin/Controllers/ProductsController.cs
+++ DoNgoaiChinhHang/DoNgoaiChinhHang/Areas/Admin/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DoNgoaiChinhHang.Areas.Admin.Models;
+using PagedList;
 
 namespace DoNgoaiChinhHang.Areas.Admin.Controllers
 {
@@ -14,54 +15,73 @@ namespace DoNgoaiChinhHang.Areas.Admin.Controllers
     {
         private DBContext db = new DBContext();
 
-        // GET: Admin/Products
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
             var products = db.Products.Include(p => p.Category);
-            return View(products.ToList());
+            products = products.OrderBy(s => s.ProductID);
+            int pageSize = 2;
+            int pageNumber = (page ?? 1);
+            return View(products.ToPagedList(pageNumber, pageSize));
         }
 
-        // GET: Admin/Products/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // GET: Admin/Products/Create
+        
         public ActionResult Create()
         {
             ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
             return View();
         }
 
-        // POST: Admin/Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProductID,CategoryID,ProductName,Image,Evaluation,Brand,Orgin,Price,Benefit,Introduction,Element,Tutorial")] Product product)
+        public JsonResult Create2(Product abc)
         {
-            if (ModelState.IsValid)
+            bool result = false;
+            var u = db.Products.Where(x => x.ProductID == abc.ProductID).FirstOrDefault();
+            if (u != null)
             {
-                db.Products.Add(product);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                result = false;
             }
+            else
+            {
+                db.Products.Add(abc);
+                db.SaveChanges();
+                result = true;
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
 
-            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
-            return View(product);
         }
 
-        // GET: Admin/Products/Edit/5
+        public JsonResult Delete(string id)
+        {
+            bool result = false;
+            var u = db.Products.Where(x => x.ProductID == id).FirstOrDefault();
+
+            if (u != null)
+            {
+                var productChilds = db.OrderDetails.Where(p => p.ProductID == id).ToList();
+                if (productChilds.Count > 0)
+                {
+                    result = false;
+                }
+                else
+                {
+                    db.Products.Remove(u);
+                    db.SaveChanges();
+                    result = true;
+                }
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Edit2(Product abc)
+        {
+            bool result = false;
+            db.Entry(abc).State = EntityState.Modified;
+            db.SaveChanges();
+            result = true;
+            return Json(result, JsonRequestBehavior.AllowGet);
+
+        }
+
+
         public ActionResult Edit(string id)
         {
             if (id == null)
@@ -77,49 +97,10 @@ namespace DoNgoaiChinhHang.Areas.Admin.Controllers
             return View(product);
         }
 
-        // POST: Admin/Products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductID,CategoryID,ProductName,Image,Evaluation,Brand,Orgin,Price,Benefit,Introduction,Element,Tutorial")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
-            return View(product);
-        }
+        
+      
 
-        // GET: Admin/Products/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = db.Products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Admin/Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            Product product = db.Products.Find(id);
-            db.Products.Remove(product);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing)
